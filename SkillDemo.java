379d3c014d7340ee0a0c403f90eef687e152553e
clarksonphan <clarksonphan@gmail.com>
2022-05-01 18:50:09 -0700

    Added methods and junit files

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,6 @@
 public class SkillDemo {
-
+    
+    public static int divide(int n, int m) {
+        return n/m;
+    }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    
+    @Test
+    public void divideTest() {
+        assertEquals(6, SkillDemo.divide(8,2));
+    }
+}
